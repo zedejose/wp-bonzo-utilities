5d6200eacc92a7212e052ab2ead06dc95882b0ec
Zé Fontainhas <jfontainhas@gmail.com>
2015-11-29 13:42:47 +0000

    Text class tweaks

--- text/text.class.php
+++ text/text.class.php
@@ -2,7 +2,7 @@
 /**
  * The WP Bonzo Utilites Text class
  *
- * Functions ro modify text strings.
+ * Functions to modify text strings.
  *
  * @package WordPress
  * @subpackage WP_Bonzo_Utilities
@@ -10,37 +10,48 @@
  *
  */
 
-Namespace WP_Bonzo_Utilities
+Namespace WP_Bonzo_Utilities;
 
 	class Text {
 
 		/**
 		 * Trim passed text to a maximum of $cut_after words.
 		 *
+		 * @param  string 	$text           		String to parse
 		 * @param  integer 	$cut_after           	How many words?
-		 * @param  string  	$read_more           	'Read More' string
+		 * @param  string  	$read_more           	'Read More' string, false if none
 		 * @param  string  	$wrap_before         	String before the returned text
 		 * @param  string  	$wrap_after          	String after the returned text
 		 * @param  bool  	$strip_html          	Strip HTML tags?
-		 * @param  array 	$allowed_tags 			List ( '<p><a>' ) of alloed HTML tags, see http://php.net/manual/en/function.strip-tags.php
+		 * @param  array 	$allowed_tags 			List ( '<p><a>' ) of allowed HTML tags, see http://php.net/manual/en/function.strip-tags.php
 		 * @return string                       	Formatted text
 		 */
-		function trim_text_to_words ( $text = '', $cut_after = 20, $read_more = '&nbsp;[...]', $wrap_before = '', $wrap_after = '', $strip_html = true, $allowed_tags = '' ) {
+		function trim_text_to_words ( $text = '', $cut_after = 20, $read_more = false, $wrap_before = '', $wrap_after = '', $strip_html = true, $allowed_tags = '' ) {
 
+			// Strip HTML, if requested
 			if ( $strip_html ) {
 				$text = strip_tags( $text, $allowed_tags );
 			}
 
+			// Transform text into array
 			$words = explode( ' ', $text );
 
-			if ( !$cut_after >= count ( $words ) ) {
-				$words 	= array_slice( $ords, 0, $cut_after );
-				$text 	= implode( ' ' , $words );
+			// Only cut if the string ins't already shorter than the $cut_after
+			if ( count ( $words ) >= $cut_after ) {
+
+				// Format Read More
+				if ( false !== $read_more ) $read_more = '&nbsp;' . $read_more;
+				// Slice to requested words
+				$words 	= array_slice( $words, 0, $cut_after );
+				// Recompose string, add $read_more
+				$text 	= implode( ' ' , $words ) . $read_more;
 
 			}
 
-			$text = $wrap_before . $text . $wrap_after . $readmore;
+			// Wrap the string
+			$text = $wrap_before . $text . $wrap_after;
 
+			// Apply filters and return
 			return apply_filters( 'wp_bonzo_trim_text_to_words', $text );
 
 		}
